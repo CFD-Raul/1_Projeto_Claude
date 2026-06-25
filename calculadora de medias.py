46df87feb157db1c5ffb33a9525fae3b688f5a50
CFD-Raul <ralemanha21@gmail.com>
2026-06-25 19:05:23 +0200

    corrigindo uso do acumulador e encadeamento do if/else

--- calculadora de medias.py
+++ calculadora de medias.py
@@ -8,25 +8,31 @@ notas = [float(nota) for nota in notas]
 
 # calculando a média
 
-soma_das_notas = sum(notas)
+soma_das_notas = 0
+for nota in notas:
+    soma_das_notas += nota
 
 media = soma_das_notas / len(notas)
 
-# gerando acumulador de acordo com o numero de notas
+# variavel para incluir na mensagem a quantidade de notas calculadas na media
 
-acumulador = len(notas)
+avaliacoes = len(notas)
 
 # classificando as notas
 
 status_do_avaliado = ""
 
-if media < 70: status_do_avaliado = "REPROVADO" 
-if media >= 70 < 80: status_do_avaliado = "C"
-if media >= 80 < 90: status_do_avaliado = "B"
-if media >= 90: status_do_avaliado = "A"
+if media < 70:
+    status_do_avaliado = "REPROVADO" 
+elif 70 <= media < 80:
+    status_do_avaliado = "C"
+elif 80 < media < 90:
+    status_do_avaliado = "B"
+else: 
+    status_do_avaliado = "A"
 
 # exibindo resultado
 
-mensagem = f"Foram avaliadas {acumulador} notas do aluno. As médias das notas avaliadas é de {media:.0f}, o aluno obteve status: {status_do_avaliado}!"
+mensagem = f"Foram avaliadas {avaliacoes} notas do aluno. As médias das notas avaliadas é de {media:.0f}, o aluno obteve status: {status_do_avaliado}!"
 
 print(mensagem)
