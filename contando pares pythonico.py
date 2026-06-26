e84d1cdd6ab9868982669ca249aa7a8a076d1e5c
CFD-Raul <ralemanha21@gmail.com>
2026-06-26 17:30:51 +0200

    criando variavel de quantidade de numeros pares

--- contando pares pythonico.py
+++ contando pares pythonico.py
@@ -6,14 +6,15 @@ numeros_do_usuario = input("Digite dez números para validação dos pares entre
 numeros_do_usuario = [int(n) for n in numeros_do_usuario]
 
 # criando a lista de números pares já validando os numeros pares
+#  NÃO HÁ USO DE ACUMULADOR NESSE CASO    
 
 numeros_pares = [numero for numero in numeros_do_usuario if numero % 2 == 0]
 
-# criando um acumulador com o valor do acumulador ja atribuido à quantidade de numeros digitados pelo usuario
+# criando uma variavel para integrar a quantidade de numeros pares à mensagem
 
-acumulador = len(numeros_pares)
+quantidade_numeros_pares = len(numeros_pares)
 
 # exibindo lista de números pares 
 
-print(f"""Há {acumulador} numeros pares dentre os digitados por você, são eles:
+print(f"""Há {quantidade_numeros_pares} numeros pares dentre os digitados por você, são eles:
 {("\n".join(map(str, numeros_pares)))}""")
