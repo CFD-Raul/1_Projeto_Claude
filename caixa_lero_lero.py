d3e83f77c4c619cb19233af575901f1631b34e4c
CFD-Raul <ralemanha21@gmail.com>
2026-06-28 11:09:50 +0200

    descrevendo processos

--- caixa_lero_lero.py
+++ caixa_lero_lero.py
@@ -1,9 +1,17 @@
+# obtendo valor
+
 valor_do_item = float(input("Digite o valor do item que deseja somar à conta: "))
 
+# Criando acumulador
+
 soma = 0
+
+# Estabelecendo o laço
 
 while valor_do_item != 0:
     soma += valor_do_item
     valor_do_item = float(input("Digite o valor do item que deseja somar à conta, caso não deseje somar nenhum outro valor digite 0: "))
+
+# exibindo resultado
 
 print(f"O total a pagar é de R${soma:.2f}")
